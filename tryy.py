566ee77d704b779e3dfa7c1b7d3755800517ef48
tush-paul <paul@example.com>
2026-05-23 16:04:15 +0530

    Add tryy.py

---
+++ tryy.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/data", methods=["POST"])
+def receive_data():
+
+    data = request.json
+
+    print("Received Data:")
+    print(data)
+
+    return {"status": "success", "message": "Data received"}
+
+
+app.run(host="0.0.0.0", port=5000)
